68ab9e0d1d77cbefc1e5ac0fd7423d34695c112a
hadeer ahmed <hader@Haders-MacBook-Pro.local>
2023-03-05 16:12:19 +0200

    add data model & func for registration

--- InStock/Model/NetworkingServices/NetworkManger.swift
+++ InStock/Model/NetworkingServices/NetworkManger.swift
@@ -10,9 +10,40 @@ import Alamofire
 
 protocol NetworkMangerProtocol {
     static func fetchData<T : Decodable>( apiLink : String ,complitionHandler: @escaping (T?) -> Void)
+    static func registerUser(registerCustomer : newCustomer, completion:@escaping (Data?, URLResponse? , Error?)->())
 }
 
 class NetworkManger : NetworkMangerProtocol {
+    
+    
+    static func registerUser(registerCustomer: newCustomer, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        var urlReq = URLRequest(url: URL(string: apiLinks.register.rawValue)!)
+        
+        urlReq.httpMethod = "POST"
+       
+        urlReq.httpShouldHandleCookies = false
+        do {
+            //asDictinonary method to cast class data model to dictionary
+            let requestBody = try JSONSerialization.data(withJSONObject: registerCustomer.asDictionary() , options: .prettyPrinted)
+            
+            urlReq.httpBody = requestBody
+            
+            urlReq.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            urlReq.addValue("application/json", forHTTPHeaderField: "Accept")
+        }catch let error {
+            print(error.localizedDescription)
+        }
+        URLSession.shared.dataTask(with: urlReq) { Data, HTTPURLResponse, error in
+            if(Data != nil && Data?.count != 0){
+                let response = String(data: Data!, encoding: .utf8)
+                print(response!)
+                print("data")
+            }
+        }.resume()
+    }
+    
+    
+    
     
     static func fetchData<T : Decodable>(apiLink : String , complitionHandler: @escaping (T?) -> Void){
 
@@ -34,10 +65,4 @@ class NetworkManger : NetworkMangerProtocol {
 }
 
 
-enum apiLinks : String {
-    
-    case Brands = "https://a546963db1d86b6cdc7f01928132e7f7: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@mad-4-ism2023.myshopify.com/admin/api/2023-01/smart_collections.json"
-    
-    
-    
-}
+
