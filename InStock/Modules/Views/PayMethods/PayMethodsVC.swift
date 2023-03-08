3f358e8da9e1849d7e963c75262f89f4e2ad6b25
m7moudelgendy <mahmoud.elgendy141@gmail.com>
2023-03-08 06:36:54 -0800

    Add Apple Pay Feature

--- InStock/Modules/Views/PayMethods/PayMethodsVC.swift
+++ InStock/Modules/Views/PayMethods/PayMethodsVC.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import PassKit
 
-class PayMethodsVC: UIViewController {
-
+class PayMethodsVC: UIViewController, PKPaymentAuthorizationViewControllerDelegate {
+    
     @IBOutlet weak var payMethodsTable: UITableView!
+    var unchecked = true
+    var payCheck = false
+    let sections = ["Online Payment","More Payment Option"]
+    var arr = [["Apple Pay"] , ["Cash On Delivery"]]
+    
+    private var paymentRequest : PKPaymentRequest = {
+        
+        let request = PKPaymentRequest()
+        request.merchantIdentifier = "merchant.iti.PaymentMethodDemo"
+        request.supportedNetworks = [.quicPay , .masterCard , .visa]
+        request.supportedCountries = ["US","EG"]
+        request.merchantCapabilities = .capability3DS
+        request.countryCode = "EG"
+        request.currencyCode = "EGP"
+        request.paymentSummaryItems = [PKPaymentSummaryItem(label: "iphone", amount: 15000)]
+        return request
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        
     }
-
+    
+    @IBAction func btnPayClicked(_ sender: Any) {
+        if (payCheck == true) {
+            applePay()
+        }else{
+            cashPay()
+        }
+    }
+    func applePay(){
+        let controller = PKPaymentAuthorizationViewController(paymentRequest: paymentRequest)
+        if controller != nil {
+            controller!.delegate = self
+            present(controller!, animated: true, completion: nil)
+        }
+    }
+    func cashPay(){
+//        let imgVC = self.storyboard?.instantiateViewController(withIdentifier: "ImageViewController") as! ImageViewController
+//        self.navigationController?.pushViewController(imgVC, animated: true)
+        
+    }
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        
+        controller.dismiss(animated: true, completion: nil)
+        
+        
+    }
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        
+        completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
+//        let imageVC = self.storyboard?.instantiateViewController(withIdentifier: "ImageViewController") as! ImageViewController
+//        self.navigationController?.pushViewController(imageVC, animated: true)
+    }
+    
+    
+    
 }
