61532f9c3b0734e5e3deae2e6c6000eede94112b
m7moudelgendy <mahmoud.elgendy141@gmail.com>
2023-03-08 06:26:55 -0800

    adding Apple Pay Capability

---
+++ InStock/Modules/Views/PayMethods/PayMethodsVC.swift
@@ -0,0 +1,19 @@
+//
+//  PayMethodsVC.swift
+//  InStock
+//
+//  Created by ElGendy on 08/03/2023.
+//
+
+import UIKit
+
+class PayMethodsVC: UIViewController {
+
+    @IBOutlet weak var payMethodsTable: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
