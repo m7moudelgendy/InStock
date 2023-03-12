87a7fa902d4a04791ab5e2b0cc8c52b18c076a00
Rezk <rezk@MacBook-Pro.local>
2023-03-12 06:33:32 -0700

    edit setting View GUI

--- InStock/Modules/Views/OrderView/OrderTableVC.swift
+++ InStock/Modules/Views/OrderView/OrderTableVC.swift
@@ -35,15 +35,13 @@ class OrderTableVC: UITableViewController , OrderViewProtocol{
         viewModel.getOrders()
     }
 
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+       
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        
         return viewModel.orders.count
     }
 
@@ -55,53 +53,19 @@ class OrderTableVC: UITableViewController , OrderViewProtocol{
         cell.detailTextLabel?.text = "Price : " + "\(viewModel.orders[indexPath.row].current_subtotal_price ?? "")"
         cell.textLabel?.font = UIFont(name: "Helvetica Neue", size: 18)
         cell.detailTextLabel?.font = UIFont(name: "Helvetica Neue", size: 16)
+        cell.backgroundColor = UIColor.white
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 8
+        cell.clipsToBounds = true
         return cell
     }
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 5
+    }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- InStock/Modules/Views/SettingsView/SettingsVC.swift
+++ InStock/Modules/Views/SettingsView/SettingsVC.swift
@@ -14,10 +14,13 @@ class SettingsVC: UIViewController {
     @IBOutlet weak var orderPriceLabel: UILabel!
     @IBOutlet weak var orderDateLabel: UILabel!
     
+    var setting : [String]?
     var viewModel : HomeViewModel!
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel = HomeViewModel()
+        
+        setting = ["Address","Currency","Contact us","About us"]
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -29,11 +32,47 @@ class SettingsVC: UIViewController {
             }
         }
         viewModel.getOrders()
-       
+        
+    }
+}
+extension SettingsVC : UITableViewDelegate , UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return setting!.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        return headerView
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = self.setting![indexPath.section]
+
+        cell.backgroundColor = UIColor.white
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 8
+        cell.clipsToBounds = true
+        
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        print("You tapped cell number \(indexPath.section).")
     }
     
     
-
-    
-
 }
+
+
+
+
