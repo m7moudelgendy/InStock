a6bd7a7a696d1d27ce5143e2c45a01e22c3d3a2e
hadeer ahmed <hader@Haders-MacBook-Pro.local>
2023-03-12 18:41:07 +0200

    add wishList View

---
+++ InStock/Modules/Views/WishListView/favTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  favTableViewCell.swift
+//  InStock
+//
+//  Created by Hader on 12/3/23.
+//
+
+import UIKit
+
+class favTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
