fb7567ca5707c6cde6e54b4839b99d15ccd7ea56
Rezk <rezk@MacBook-Pro.local>
2023-03-11 17:31:53 -0800

    Add Settings View

---
+++ InStock/Modules/Views/SettingsView/SettingsVC.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsVC.swift
+//  InStock
+//
+//  Created by Rezk on 11/03/2023.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
