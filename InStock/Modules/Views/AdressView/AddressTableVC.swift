8e17c817ac7f0f728e3a334c7706dd29777aa4be
m7moudelgendy <mahmoud.elgendy141@gmail.com>
2023-03-15 06:13:45 -0700

    handle No Addresses In Address Table

--- InStock/Modules/Views/AdressView/AddressTableVC.swift
+++ InStock/Modules/Views/AdressView/AddressTableVC.swift
@@ -45,8 +45,12 @@ class AddressTableVC: UITableViewController,TableViewProtocol {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        
+        if viewModel.result.count == 0 {
+        tableView.setEmptyAddressView(title: "You don't have any Shipping Address", message: "You Must Add Address.")
+        }
+        else {
+        tableView.restoreAddress()
+        }
         return viewModel.result.count
     }
 
@@ -103,7 +107,37 @@ class AddressTableVC: UITableViewController,TableViewProtocol {
         present(alert, animated: true)
         
     }
-    
-    
 }
+extension UITableView {
+    func setEmptyAddressView(title: String, message: String) {
+        let emptyView = UIView(frame: CGRect(x: self.center.x, y: self.center.y, width: self.bounds.size.width, height: self.bounds.size.height))
+        let titleLabel = UILabel()
+        let messageLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = UIColor.black
+        titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 18)
+        messageLabel.textColor = UIColor.lightGray
+        messageLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
+        emptyView.addSubview(titleLabel)
+        emptyView.addSubview(messageLabel)
+        titleLabel.centerYAnchor.constraint(equalTo: emptyView.centerYAnchor).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+        messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+        messageLabel.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 20).isActive = true
+        messageLabel.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -20).isActive = true
+        titleLabel.text = title
+        messageLabel.text = message
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        // The only tricky part is here:
+        self.backgroundView = emptyView
+        self.separatorStyle = .none
+    }
+    func restoreAddress() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
+    }
+}
+
 
